ac5d534f2465e5333c4b01b50435cc453878952a
Damian <damian.plomp@student.hu.nl>
2023-05-17 15:22:57 +0200

    Files added

--- warehouse/main.cpp
+++ warehouse/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "src/include/Employee.hpp"
+// #include "src/include/Pallet.hpp"
+// #include "src/include/Shelf.hpp"
+#include "src/include/Warehouse.hpp"
 
-int main(void){
-    std::cout << "Hello world" << std::endl;
-}
+int main(void)
+{
+    Employee p1 = Employee("Jantje", true, true);
+
+    std::cout << p1.GetName();
+};
